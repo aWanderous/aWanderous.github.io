5e534ddfafa5a5404b86b6bcdc7716d9bc101374
Alex Wan <a_wanderous@hotmail.com>
2019-09-29 00:52:15 +1000

    moved all crystal elements to each folder

---
+++ assets/css/game.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+
+  // target randomly generated
+  var target = Math.floor(Math.random() * 120) + 19;
+  $("#getNumber").html(target);
+
+  // button clicked gives a random value for each button pressed
+  var lego = Math.floor(Math.random() * 12) + 1;
+  var plastic = Math.floor(Math.random() * 12) + 1;
+  var stones = Math.floor(Math.random() * 12) + 1;
+  var treasure = Math.floor(Math.random() * 12) + 1;
+  var counter = 0;
+  $("#score").html("Please help!!")
+  $("#collected").html("Please hurry!!")
+  $("#again").html("of my lord's crystals.")
+
+  var wins = 0;
+  var loses = 0;
+
+  // when boxes are clicked add the value together
+  $("#legoBox, #plasticBox, #stonesBox, #treasureBox").on("click", function () {
+    var buttonId = $(this).attr('id');
+    
+    switch(buttonId) {
+      case "legoBox":
+      counter = counter + lego;
+      $("#collected").html("Great!! we have now collected " + counter + "!");
+      break;
+    
+      case "plasticBox":
+      counter = counter + plastic;
+      $("#collected").html("Great!! we have now collected " + counter + "!");
+      break;
+
+      case "stonesBox":
+      counter = counter + stones;
+      $("#collected").html("Great!! we have now collected " + counter + "!");
+      break;
+
+      case "treasureBox":
+      counter = counter + treasure;
+      $("#collected").html("Great!! we have now collected " + counter + "!");
+      break;
+
+    } 
+    // if the value added together == the same as target number user wins
+    if (counter === target) {
+      wins++
+      reset ();
+      $("#result").html("Well done!! You got the crystals back!")
+    }
+    // if the value does not add up to same target number then the user loses
+    if (counter > target) {
+      loses++
+      reset ();
+      $("#result").html("The bandits have chased you out!!");
+    }
+    
+  });
+  // reset values
+  function reset() {
+    target = Math.floor(Math.random() * 120) + 19;
+    $("#getNumber").html(target);
+   lego = Math.floor(Math.random() * 12) + 1;
+   plastic = Math.floor(Math.random() * 12) + 1;
+   stones = Math.floor(Math.random() * 12) + 1;
+   treasure = Math.floor(Math.random() * 12) + 1;
+   counter = 0;
+   $("#again").html("of my lord's crystals again.")
+   $("#score").html("We have collected back " + wins + " times, but the bandits have beaten us " + loses + " times.");
+   $("#collected").html("If only someone will do something about these bandits.")
+  }
+
+});
+
